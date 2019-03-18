e002a87a6c160c33cc81493ea202c560db54431a
Nnamdi Okeke <kingsleyob2@gmail.com>
2019-03-18 11:13:03 -0400

    inputs some code

---
+++ logic_gates.py
@@ -0,0 +1,11 @@
+# Setting up th varibales x, y
+
+x = int(input("Enter a number 1, 0: "))
+y = int(input("Enter a number 1, 0: "))
+
+# creating the logic for the and gate 
+
+#AND gate
+if (x == 1 and y == 1):
+    ANDval = "True"
+    ANDgate = 1 
